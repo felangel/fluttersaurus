98d19d2ef6f6af7e1bb4fc3f3b31134d28c014a8
Felix Angelov <felangelov@gmail.com>
2020-08-12 21:53:44 -0500

    refactor: simplify search page

--- lib/search/view/search_page.dart
+++ lib/search/view/search_page.dart
@@ -8,39 +8,35 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 48),
-                child: Row(
-                  children: [
-                    Image.asset('assets/icons/fluttersaurus.png', width: 48),
-                    const SizedBox(width: 8),
-                    Text(
-                      'Fluttersaurus',
-                      style: textTheme.headline4.copyWith(
-                        color: Colors.black87,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 24),
-              Flexible(
-                child: BlocProvider(
-                  create: (context) => SearchBloc(
-                    context.repository<ThesaurusRepository>(),
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.asset('assets/icons/fluttersaurus.png', width: 48),
+                const SizedBox(width: 8),
+                Text(
+                  'Fluttersaurus',
+                  style: textTheme.headline4.copyWith(
+                    color: Colors.black87,
                   ),
-                  child: SearchForm(),
                 ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            Flexible(
+              child: BlocProvider(
+                create: (context) => SearchBloc(
+                  context.repository<ThesaurusRepository>(),
+                ),
+                child: SearchForm(),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
